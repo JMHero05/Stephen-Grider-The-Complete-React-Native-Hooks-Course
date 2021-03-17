d72b8a1e68e3c75bca892b51f1d1e605e055f00a
Jake Mills <jake.mills.05@gmail.com>
2021-03-17 17:49:30 -0400

    First attempt at FlatList

--- rn-starter/src/screens/ListScreen/index.js
+++ rn-starter/src/screens/ListScreen/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
 const ListScreen = () => {
+  const friends = [
+    { name: 'Teresa' },
+    { name: 'Jake' },
+    { name: 'Mark' },
+    { name: 'Glenda' },
+    { name: 'Melinda' },
+    { name: 'David' },
+    { name: 'Tess' },
+    { name: 'Ross' },
+    { name: 'Matt' },
+    { name: 'Olivia' },
+  ];
+
   return (
     <View>
-      <Text>List Screen</Text>
+      <FlatList
+        data={friends}
+        renderItem={({ item }) => {
+          return <Text key={item.name}>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
