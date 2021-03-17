a774b42ac1e70e09d7ffa1754a99b23259cd5912
Jake Mills <jake.mills.05@gmail.com>
2021-03-17 13:53:15 -0400

    Created components screen and separate style folder/file

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { styles } from '../styles/ComponentsScreenStyles';
+
+const ComponentsScreen = () => {
+  return <Text style={styles.textStyle}>This is the components screen</Text>;
+};
+
+export default ComponentsScreen;
