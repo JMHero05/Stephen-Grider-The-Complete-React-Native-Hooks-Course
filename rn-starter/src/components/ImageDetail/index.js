b7f4a1f228cc2bd31b6af55945351a4c1bbda424
Jake Mills <jake.mills.05@gmail.com>
2021-03-17 19:25:15 -0400

    Created Image Detail Component

---
+++ rn-starter/src/components/ImageDetail/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ImageDetail = () => {
+  return (
+    <View>
+      <Text>Image Detail</Text>
+    </View>
+  );
+};
+
+export default ImageDetail;
