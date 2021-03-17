aaf9df9946388e45d850577e9026d833a871668b
Jake Mills <jake.mills.05@gmail.com>
2021-03-17 19:19:55 -0400

    Created Image Screen and added navigation from home screen via button

---
+++ rn-starter/src/screens/ImageScreen/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <Text>Image Screen!</Text>
+    </View>
+  );
+};
+
+export default ImageScreen;
