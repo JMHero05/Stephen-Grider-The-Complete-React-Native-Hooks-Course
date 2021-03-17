e107ddfeb891945c35279c908560ecef674220eb
Jake Mills <jake.mills.05@gmail.com>
2021-03-17 18:50:11 -0400

    Refactored styling to own file

--- rn-starter/src/screens/HomeScreen/index.js
+++ rn-starter/src/screens/HomeScreen/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text } from 'react-native';
+import { styles } from './styles';
 
 const HomeScreen = () => {
   return <Text style={styles.text}>Hello World!</Text>;
 };
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-  },
-});
 
 export default HomeScreen;
