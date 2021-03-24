4c10d28bce559317487f9a566156c6653277681f
Jake Mills <jake.mills.05@gmail.com>
2021-03-24 16:13:32 -0400

    Imported ColorCounter to Square Screen and added three iterations of it

--- rn-starter/src/screens/SquareScreen/index.js
+++ rn-starter/src/screens/SquareScreen/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import ColorCounter from '../../components/ColorCounter';
 
 const SquareScreen = () => {
   return (
     <View>
-      <Text>Square Screen</Text>
+      <ColorCounter color='Red' />
+      <ColorCounter color='Blue' />
+      <ColorCounter color='Green' />
     </View>
   );
 };
