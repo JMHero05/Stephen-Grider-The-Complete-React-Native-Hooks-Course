2571d43ade1c8a3eb95835186fa59552fd43123d
Jake Mills <jake.mills.05@gmail.com>
2021-03-24 15:16:57 -0400

    Created Color Screen with Button

---
+++ rn-starter/src/screens/ColorScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Button title='Add a Color' />
+    </View>
+  );
+};
+
+export default ColorScreen;
