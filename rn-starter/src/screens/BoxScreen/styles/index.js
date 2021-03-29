4f598f63c0b5cfb966e91e532bfc69b91efbc6d0
Jake Mills <jake.mills.05@gmail.com>
2021-03-29 17:42:13 -0400

    Played around with flexbox: justifyContent

--- rn-starter/src/screens/BoxScreen/styles/index.js
+++ rn-starter/src/screens/BoxScreen/styles/index.js
@@ -6,6 +6,7 @@ export const styles = StyleSheet.create({
     borderColor: 'black',
     flexDirection: 'row',
     height: 200,
+    justifyContent: 'flex-start',
   },
   textStyle: {
     borderWidth: 3,
