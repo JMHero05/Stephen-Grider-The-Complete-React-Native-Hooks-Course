0f4651b83ca3ee23fee626085e246be41aee6e65
Jake Mills <jake.mills.05@gmail.com>
2021-03-29 17:32:33 -0400

    Imported Styles and played with some styles

--- rn-starter/src/screens/BoxScreen/index.js
+++ rn-starter/src/screens/BoxScreen/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { styles } from './styles';
 
 const BoxScreen = () => {
+  const { viewStyle, textStyle } = styles;
   return (
-    <View>
-      <Text></Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>Box Screen</Text>
     </View>
   );
 };
--- rn-starter/src/screens/BoxScreen/styles/index.js
+++ rn-starter/src/screens/BoxScreen/styles/index.js
@@ -1,5 +1,13 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-  style,
+export const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+  },
+  textStyle: {
+    borderWidth: 10,
+    borderColor: 'red',
+    margin: 20,
+  },
 });
