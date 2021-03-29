35e068a756471345bef1ebc1367fd642a800d627
Jake Mills <jake.mills.05@gmail.com>
2021-03-29 16:30:37 -0400

    Created textInput and imported styling

--- rn-starter/src/screens/TextScreen/index.js
+++ rn-starter/src/screens/TextScreen/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { styles } from './styles';
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>Text Screen</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 };
