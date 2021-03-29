1fe96bc3d9cf8c9cc4ef55774e65ea410e8d4466
Jake Mills <jake.mills.05@gmail.com>
2021-03-29 16:44:37 -0400

    Added state to properly use textInput

--- rn-starter/src/screens/TextScreen/index.js
+++ rn-starter/src/screens/TextScreen/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput } from 'react-native';
 import { styles } from './styles';
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
+
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text>Hi, my name is {name}</Text>
     </View>
   );
 };
