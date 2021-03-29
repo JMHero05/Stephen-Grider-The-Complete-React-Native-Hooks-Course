0ccccf65486c9cd0ba07e5c1822e0508fa71264e
Jake Mills <jake.mills.05@gmail.com>
2021-03-29 16:20:50 -0400

    Created template for TextScreen and styles folder

---
+++ rn-starter/src/screens/TextScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TextScreen = () => {
+  return (
+    <View>
+      <Text>Text Screen</Text>
+    </View>
+  );
+};
+
+export default TextScreen;
